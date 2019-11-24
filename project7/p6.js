904c7255c1241ff6877e8703b44552d53410060a
Yiwei Zhang <happyzyw@umich.edu>
2019-11-24 17:53:50 -0600

    localstorage main

--- project7/p6.js
+++ project7/p6.js
@@ -1,31 +1,42 @@
 
 var name_list=[];
-var loca=document.getElementById("event_location").value;
-var memo=document.getElementById("event_memo").value;
-var name=document.getElementById("event_name").value;
-var hour=document.getElementById("hour").value;
-var minute=document.getElementById("minute").value;
+var loca;
+var memo;
+var name;
+var hour;
+var minute;
 var minute_back;
 var hour_back;
 var name_back;
 var loca_back;
 var memo_back;
-var date=document.getElementById("date").value;
+var date;
 var date_back;
 
 
+// function for redirect to main page
 function home() {
-    window.location.href = 'main.html';
+    window.location = "./main.html";
+}
+
+// refresh the main page
+// this function should rendering all element in main page
+function refresh_main() {
+    console.log("inside home", name);
+    document.getElementById("event-name").innerHTML = name;
 }
 
 
 function save(){
-	date=document.getElementById("date").value;
-	hour=document.getElementById("hour").value;
-    minute=document.getElementById("minute").value;
-	name=document.getElementById("event_name").value;
-	loca=document.getElementById("event_location").value;
-    memo=document.getElementById("event_memo").value;
+    // date=document.getElementById("date").value;
+    // hour=document.getElementById("hour").value;
+    // minute=document.getElementById("minute").value;
+    name = document.getElementById("event_name").value;
+    console.log("name", name);
+    // loca=document.getElementById("event_location").value;
+    // memo=document.getElementById("event_memo").value;
+    localStorage.setItem(name, memo);
+    home();
 }
 
 function passvalueinadd(){
@@ -52,21 +63,21 @@ loca_back=key[4];
 memo_back=key[5];
   // document.getElementById("event-information").innerHTML="date:"+date_back+"&nbsp;&nbsp;time:"+hour_back+":&nbsp;"+minute_back+"\n"+"name:"+name_back+"&nbsp;&nbsp;location:"+loca_back+"&nbsp;&nbsp;memo:"+memo_back;
   document.getElementById("event-name").innerHTML = name_back;
-    document.getElementById("event-time").innerHTML = date_back;
-    document.getElementById("event-time").innerHTML = loca_back;
+  document.getElementById("event-time").innerHTML = date_back;
+  document.getElementById("event-time").innerHTML = loca_back;
 }
 	
 }
 
 function add(){
-	window.location.href='add.html?'+date_back+"&"+hour_back+"&"+minute_back+"&"+name_back+"&"+loca_back+"&"+memo_back;
-	
+	// window.location.href='add.html?'+date_back+"&"+hour_back+"&"+minute_back+"&"+name_back+"&"+loca_back+"&"+memo_back;
+    window.location.href = "./add.html";
 }
 
-
-function refresh(){
-	manyValues();
-}
+//
+// function refresh(){
+// 	manyValues();
+// }
 
 function selectChange()
 {
