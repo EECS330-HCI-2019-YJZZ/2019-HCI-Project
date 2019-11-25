5002ef109bd212a74041366327ebbdc76d49dff7
Yiwei Zhang <happyzyw@umich.edu>
2019-11-24 20:09:15 -0600

    multiple event set up

--- project7/p6.js
+++ project7/p6.js
@@ -1,19 +1,25 @@
-
-var name_list=[];
 var loca;
 var memo;
 var name;
 var hour;
 var minute;
-var minute_back;
-var hour_back;
-var name_back;
-var loca_back;
-var memo_back;
 var date;
-var date_back;
-var info = [];
 
+// clone the repeated structure many times
+function render(node) {
+    var cnt = localStorage.getItem("count");
+    if (cnt === null) {
+        return;
+    }
+    else {
+        cnt = parseInt(cnt);
+        cnt++;
+        for (var i = 0; i < cnt - 1; i++) {
+            copy = node.cloneNode(true);
+            node.parentNode.insertBefore(copy, node);
+        }
+    }
+}
 
 // refresh the main page
 // this function should rendering all element in main page
@@ -21,6 +27,7 @@ function refresh_main() {
     console.log("inside home", name);
     document.getElementById("event-name").innerHTML = name;
     console.log(localStorage);
+    render(document.querySelector('.main-event'), 4);
 }
 
 // save the variable to localStorage and redirect to main page
@@ -50,71 +57,17 @@ function save(){
     window.location = "./main.html";
 }
 
-function passvalueinadd(){
-window.location.href='main.html?'+date+"&"+hour+"&"+minute+"&"+name+"&"+loca+"&"+memo;
-}
-
-function manyValues(){
-var url=window.location.search;
-if(url.indexOf("?")!=-1){
-var str = url.substr(1);
-   strs = str.split("&"); 
-   var key=new Array(strs.length);
-   var value=new Array(strs.length);
-   for(i=0;i<strs.length;i++){
-    key[i]=strs[i].split("=")[0]
-    value[i]=unescape(strs[i].split("=")[1]); 
-    if (key[i]==""){key[i]="&nbsp;"}
-  } 
-date_back=key[0];
-hour_back=key[1];
-minute_back=key[2];
-name_back=key[3];
-loca_back=key[4];
-memo_back=key[5];
-  // document.getElementById("event-information").innerHTML="date:"+date_back+"&nbsp;&nbsp;time:"+hour_back+":&nbsp;"+minute_back+"\n"+"name:"+name_back+"&nbsp;&nbsp;location:"+loca_back+"&nbsp;&nbsp;memo:"+memo_back;
-  document.getElementById("event-name").innerHTML = name_back;
-  document.getElementById("event-time").innerHTML = date_back;
-  document.getElementById("event-time").innerHTML = loca_back;
-}
-	
-}
-
 function add(){
 	// window.location.href='add.html?'+date_back+"&"+hour_back+"&"+minute_back+"&"+name_back+"&"+loca_back+"&"+memo_back;
     window.location.href = "./add.html";
 }
 
-//
-// function refresh(){
-// 	manyValues();
-// }
+function edit(){
 
-function selectChange()
-{
-  var sel=document.getElementById("hour");
-  var option = new option("Text","hour");
-  sel.add(option);
-}  
-
-function manyValuesinadd(){
-var url=window.location.search;
-if(url.indexOf("?")!=-1){
-var str = url.substr(1);
-   strs = str.split("&"); 
-   var key=new Array(strs.length);
-   var value=new Array(strs.length);
-   for(i=0;i<strs.length;i++){
-    key[i]=strs[i].split("=")[0]
-    value[i]=unescape(strs[i].split("=")[1]); 
-  } 
 }
-document.getElementById("date").value=key[0];
-document.getElementById("hour").value=key[1];
-document.getElementById("minute").value=key[2];
-document.getElementById("event_name").value=key[3];
-document.getElementById("event_location").value=key[4];
-document.getElementById("event_memo").value=key[5];
+
+function delete_item(){
+
 }
 
 
